b2aeb4a11c69fc9f451d66fb144d1b105fbf0f29
Diego Bonilha <diego_martins2016@hotmail.com>
2025-01-29 18:07:10 -0400

    refactor: remove validacoes desnecessarias

--- src/Validators/ValidaEmpresa.php
+++ src/Validators/ValidaEmpresa.php
@@ -21,20 +21,7 @@ class ValidaEmpresa
     {
         $data = $pessoa->toArray();
         $validacao = v::arrayVal()->allOf(
-            v::key('cpfCnpj', v::cnpj()->notEmpty()),
-            v::key('razaoSocial', v::stringType()->notEmpty()),
-            v::key('simplesNacional', v::boolType()->notEmpty()),
-            v::key('regimeTributario', v::numericVal()->notOptional()),
-            v::key('regimeTributarioEspecial', v::numericVal()->notOptional()),
-            v::key('inscricaoMunicipal', v::stringType()),
-            v::key('inscricaoEstadual', v::stringType()),
-            v::key('email', v::stringType()),
-            v::keyNested('endereco.bairro', v::stringType()->notEmpty()),
-            v::keyNested('endereco.cep', v::stringType()->notEmpty()),
-            v::keyNested('endereco.codigoCidade', v::stringType()->notEmpty()),
-            v::keyNested('endereco.estado', v::stringType()->notEmpty()),
-            v::keyNested('endereco.logradouro', v::stringType()->notEmpty()),
-            v::keyNested('endereco.tipoLogradouro', v::stringType()->notEmpty()),
+            v::key('cpfCnpj', v::cnpj())
         )->validate($data);
 
         if(!$validacao){
